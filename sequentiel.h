237a0dcce87d856624e702448a14332987511c31
wlker-mk <isymalter@gmail.com>
2025-02-05 14:05:52 +0000

    Update sequentiel.h
    >Gestion des caracteres avec des accents
    >Ajout d'une cartouche pour la fonction enregestriter_pseudo()

--- sequentiel.h
+++ sequentiel.h
@@ -1,14 +1,18 @@
 /*  Name : sequentiel.h
     Purpose : Structure et prototypes des fonctions se rapportant
-            � la cr�ation du fichier .txt cr��
-    Authors : TCHAMIE Bindounow Gnimdou Jepht� & AGBAKOSSI Ekou� Seyram R�n�
+            à la création du fichier .txt créé
+    Authors : TCHAMIE Bindounow Gnimdou Jephté & AGBAKOSSI Ekoué Seyram Réné
     Date    : 29/11/2024
 */
 
 void enregistrer_tentatives(const char *pseudo, const int *tentatives, int nombre_tentatives, double ecart_type, int difficulte);
-/*  But : Cr� et enr�gistre les pseudos et les scores
-    Parametres: le pseudo, le nombre de tentative, les tentatives entr�es, l'�cart type
+/*  But : Cré et enrégistre les pseudos et les scores
+    Parametres: le pseudo, le nombre de tentative, les tentatives entrées, l'écart type
     Return: Rien
  */
 
 void enregistrer_pseudo(const char *pseudo, const char *password);
+/*  But : Cré et enrégistre les pseudos et les mots de passe
+    Parametres: le pseudo, le mot de passe 
+    Return: Rien
+ */
