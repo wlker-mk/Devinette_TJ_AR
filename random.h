8a20cf18e8147c9c99e3d04122a3cf035cdc433b
wlker-mk <isymalter@gmail.com>
2025-02-05 14:02:10 +0000

    Update random.h
    Gestion des caracteres avec accent

--- random.h
+++ random.h
@@ -1,24 +1,24 @@
 /*  Name : sequentiel.h
     Purpose : Structure et prototypes des fonctions se rapportant
-            � la manipulation et la gestion du fichier .txt cr��
-    Authors : TCHAMIE Bindounow Gnimdou Jepht� & AGBAKOSSI Ekou� Seyram R�n�
+            à la manipulation et la gestion du fichier .txt créé
+    Authors : TCHAMIE Bindounow Gnimdou Jephté & AGBAKOSSI Ekoué Seyram Réné
     Date    : 29/11/2024
 */
 
 int pseudo_existe(const char *pseudo);
-/*  But : V�rifie si le pseudo entr� existe d�ja
+/*  But : Vérifie si le pseudo entré existe déja
     Parametres: le pseudo
     Return: La valeur 0
  */
 
 int password_correct(const char *pseudo, const char *password);
-/*  But : V�rifie si le mot de passe entr� est correct
+/*  But : Vérifie si le mot de passe entré est correct
     Parametres: le pseudo, le mot de passe
     Return: La valeur 0
  */
 
 void afficher_scores();
 /*  But : Affiche l'historique des pseudo et des utilisateurs
-    Parametres: le pseudo, le nombre de tentative, les tentatives entr�es, l'�cart type
+    Parametres: le pseudo, le nombre de tentative, les tentatives entrées, l'écart type
     Return: Rien
  */
